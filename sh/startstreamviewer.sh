069cd8158c84fe2ca37d29982683f4ca03d8f3c7
William Wash <william.wash@sas.com>
2019-03-29 12:54:59 -0400

    Update startstreamviewer.sh

--- sh/startstreamviewer.sh
+++ sh/startstreamviewer.sh
@@ -1,1 +1,1 @@
-nohup $DFESP_HOME/bin/streamviewer.sh -product h2 -http 8990 -h2file /home/sas/forrester/data/config -noauth >> /home/sas/forrester/streamviewer.log 2>&1 &
+nohup $DFESP_HOME/bin/streamviewer.sh -product h2 -http 8990 -h2file ~/streamviewerconfig -noauth >> ~/streamviewer.log 2>&1 &
