ca19500fb4a81dd4cfd3c6ffb9ba92013ed9b76d
William Wash <william.wash@sas.com>
2019-03-29 12:49:57 -0400

    Update deleteprojects.sh

--- sh/deleteprojects.sh
+++ sh/deleteprojects.sh
@@ -1,1 +1,3 @@
-dfesp_xml_client -url "http://localhost:3389/SASESP/projects/BankingLimitManagement" -delete
+project="BankingLimitManagement"
+
+dfesp_xml_client -url "http://localhost:3389/SASESP/projects/${project}" -delete
